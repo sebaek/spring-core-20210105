1bd2ed593a865f5ea0d0c3574e6b982cee615170
sebaek <sebaek@gmail.com>
2021-01-06 10:15:15 +0900

    ex2

--- src/spring_ex2/Main.java
+++ src/spring_ex2/Main.java
@@ -9,6 +9,19 @@ public class Main {
 				new ClassPathXmlApplicationContext("spring_ex2/context.xml");
 		
 		
+//		spring_ex2.Tire tire1 = new spring_ex2.Tire();
+//		spring_ex2.Car car = new spring_ex2.Car(tire1);
+		
+		Object b1 = context.getBean("tire1");
+		System.out.println(b1);
+		
+		Object b2 = context.getBean("car");
+		System.out.println(b2);
+		
+		Car c1 = (Car) b2;
+		System.out.println(c1.getTire());
+		
+		
 		
 	}
 }
